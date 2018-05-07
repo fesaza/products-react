14ebc610cd7419f730fd4a61a1f3cea6484423ac
fesaza <fesaza@gmail.com>
2018-05-07 15:14:35 -0500

    ES6

--- src/actions/ClientsActions.js
+++ src/actions/ClientsActions.js
@@ -1,10 +1,9 @@
 import {TypesActions} from './TypesActions';
 let nextClientId = 0
-export const addClient = client => {
- return {type: TypesActions.CREATE_CLIENT, id: nextClientId++, client}
-}
+export const addClient = client => (
+ {type: TypesActions.CREATE_CLIENT, id: nextClientId++, client}
+);
 
-export const filterClient = filter => {
- debugger;
- return {type: TypesActions.FILTER_CLIENT, filter}
-}
+export const filterClient = filter => (
+ {type: TypesActions.FILTER_CLIENT, filter}
+);
